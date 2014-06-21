89b466bd16f5bf08ed0f8624cc697ee745166a94
mauricio <karbunkle@gmail.com>
2014-06-21 19:15:52 -0300

    crear barco

--- src/main/java/uy/edu/ort/arqliv/obligatorio/web/controllers/ShipController.java
+++ src/main/java/uy/edu/ort/arqliv/obligatorio/web/controllers/ShipController.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -30,13 +33,9 @@ public class ShipController {
 	@Autowired
 	private ShipService shipService;
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public String list(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
 		List<Ship> ships = new ArrayList<>();
 		try {
 			ships = shipService.list("rodrigo");
@@ -45,6 +44,26 @@ public class ShipController {
 		}
 		model.addAttribute("ships", ships );
 		
+		return "ships/list";
+	}
+	
+	@RequestMapping(value = "/create", method = RequestMethod.GET) 
+	public String setupCreate(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		model.addAttribute("ship", new Ship());
+		return "ships/create"; 
+	}
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public String submitCreate(@Valid Ship newShip, BindingResult result) {
+		if(result.hasErrors()) {
+            return "ships/create";
+        }
+		try {
+			shipService.store("rodrigo", newShip);
+		} catch (CustomServiceException e) {
+			e.printStackTrace();
+		}
 		return "ships/list";
 	}
 	
